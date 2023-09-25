262c1f2753a7212be52a48c6a15bfd3e0e8aa794
zwangMcGill <ziming.wang2@mcgill.ca>
2023-09-24 23:58:48 -0400

    with maven

--- src/main/java/ca/mcgill/cs/processor/SimpleAnnotationProcessor.java
+++ src/main/java/ca/mcgill/cs/processor/SimpleAnnotationProcessor.java
@@ -1,29 +1,12 @@
 package ca.mcgill.cs.processor;
-import ca.mcgill.cs.utils.Method;
-import ca.mcgill.cs.utils.*;
 
-import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.BodyDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.TypeDeclaration;
-import com.github.javaparser.ast.expr.AnnotationExpr;
-import com.github.javaparser.ast.expr.NormalAnnotationExpr;
 import com.google.auto.service.AutoService;
 
-import javax.annotation.processing.*;
-import javax.lang.model.element.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import org.junit.jupiter.api.Test;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
 
 @SuppressWarnings("all")
 //@SupportedAnnotationTypes("Tag")
